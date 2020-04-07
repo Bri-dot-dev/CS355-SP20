063fed77f930742058145c87c3d4fbb6d9d943bc
Stan Kurkovsky <stankurkovsky@gmail.com>
2020-04-07 12:49:44 -0400

    added write example

---
+++ 0407_write/write.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+
+#define BUFSIZE 1024
+
+int main(int ac, char *av[]){
+    if(ac!=2)
+        fprintf(stderr, "usage: %s ttyname\n", av[0]);
+    else {
+        int fd=open(av[1], O_WRONLY);
+        if(fd==-1)
+            perror(av[1]);
+        else {
+            char buf[BUFSIZE];
+            while(fgets(buf, BUFSIZE, stdin)!=0) {
+                if(write(fd, buf, strlen(buf))==-1)
+                    break;
+            }
+            close(fd);
+        }
+    }
+}
